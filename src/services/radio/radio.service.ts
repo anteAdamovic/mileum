6577ac62395336677239ebc0e7587cfcb03dcbd6
Ante <ante.adamovic93@gmail.com>
2017-12-14 19:51:35 +0100

    Add some comments and log info.

--- src/services/radio/radio.service.ts
+++ src/services/radio/radio.service.ts
@@ -10,10 +10,18 @@ export class Radio {
         this.channels['general'].emit(signal);
     }
 
+    /**
+     * Broadcast the given signal to the specified channel,
+     * if the channel doesn't exist it will create a new one
+     * before sending the signal.
+     * @param channel - Channel name.
+     * @param signal - Signal to be sent.
+     */
     public broadcastToChannel(channel: string, signal: Signal) {
         if (!this.channels[channel]) {
             this.channels[channel] = new EventEmitter<Signal>();
         }
+        console.warn('Sending a signal to newly created channel ', channel, '. Message won\'t be received without active subscriber.');
         this.channels[channel].emit(signal);
     }
 
@@ -31,6 +39,8 @@ export class Radio {
     public addChannel(channel: string): void {
         if(!this.channels[channel]) {
             this.channels[channel] = new EventEmitter<Signal>();
+        } else {
+            console.warn('Trying to add an already existing channel - ', channel);
         }
     }
 }
@@ -46,7 +56,7 @@ export class Signal {
 
     static fromObject(object: any): Signal {
         if (!object) {
-            throw new Error("Object is null or undefined at Signal.fromObject()");
+            throw new Error('Object is null or undefined at Signal.fromObject()');
         }
         return new Signal(object.name, object.message);
     }
